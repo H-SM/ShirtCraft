dc698120a8b741733ee4523bc071d2f176dc9e47
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-07-29 17:23:22 +0530

    Finshed basic Shirt and CameraRig

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -37,7 +37,7 @@ const CameraRig = ({ children }) => {
             [state.pointer.y /10 ,-state.pointer.x /5, 0],
             0.25, delta
         )
-    })
+    });
     return (
         <group ref={group}>
           {children}
